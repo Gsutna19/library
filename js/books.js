fb1e279d5d1e5f43973d99572120263f74d64cd8
Gsutna19 <fe.lipe19menezes@gmail.com>
2022-12-16 12:22:27 -0500

    Style book cards to display in library.

--- js/books.js
+++ js/books.js
@@ -17,13 +17,13 @@ document.getElementById("addBook").addEventListener("submit", function(e) {
 
 // To change read status of book
 Book.prototype.toggleRead = function() {
-    if (this.read == "read") {
-        this.read = "not read"
+    if (this.read == "Read") {
+        this.read = "Not Read"
     } else {
-        this.read = "read"
+        this.read = "Read"
     }
 
-    console.log(this.read)
+    // console.log(this.read)
     return this.read
 }
 
@@ -38,63 +38,74 @@ function addBookToLibrary() {
     } else if (document.getElementById("n").checked) {
         newRead = document.getElementById("n").value;
     }
-
+    
     const newBook = new Book(newTitle, newAuthor, newPages, newRead)
-
+    
     myLibrary.push(newBook)
     document.getElementById("addBook").reset();
-
-    console.table(myLibrary)
+    
+    // console.table(myLibrary)
 }
 
 function displayLibrary() {
+    // Clear #library innerHTML
+    document.getElementById("library").innerHTML = "";
     if (myLibrary.length === 0){
-        document.getElementById("library").innerHTML = "<p>Your Library is empty! Add a new book!</p>";
+        document.getElementById("library").innerHTML = "<p class='msg'>Your Library is empty! Add a new book!</p>";
     } else {
         for (let i = 0; i < myLibrary.length; i++) {
             // Create Card
             let card = document.createElement("div");
-            card.classList.add = "card";
+            card.classList.add("card");
             card.setAttribute("id", i);
             card.setAttribute("data-num", i);
             // To create title text and display it inside card
             let bookTitle = document.createElement("p");
-            bookTitle.classList.add = "title";
+            bookTitle.classList.add("title");
             let titleText = document.createTextNode(myLibrary[i].title)
             bookTitle.appendChild(titleText);
             card.appendChild(bookTitle);
             // To create author text and display it inside card
             let bookAuthor = document.createElement("p");
-            bookAuthor.classList.add = "author";
-            let authorText = document.createTextNode(myLibrary[i].author)
+            bookAuthor.classList.add("author");
+            let authorText = document.createTextNode(`by ${myLibrary[i].author}`)
             bookAuthor.appendChild(authorText);
             card.appendChild(bookAuthor);
             // To create pages text and display it inside card
             let bookPages = document.createElement("p");
-            bookPages.classList.add = "pages";
-            let pagesText = document.createTextNode(myLibrary[i].pages)
+            bookPages.classList.add("pages");
+            let pagesText = document.createTextNode(`${myLibrary[i].pages} pages`)
             bookPages.appendChild(pagesText);
             card.appendChild(bookPages);
             // To create "read/not read" text and display it inside card
             let bookRead = document.createElement("p");
-            bookRead.classList.add = "read";
+            bookRead.classList.add("read");
             let readText = document.createTextNode(myLibrary[i].read)
             bookRead.appendChild(readText);
             card.appendChild(bookRead);
             // Toggle read status button
             let button = document.createElement("button");
-            let btnText = document.createTextNode("Change Read status")
+            button.classList.add("sec-btn");
+            let btnText = document.createTextNode("Change Status")
             button.appendChild(btnText);
             button.addEventListener("click", function() {
                 readText.nodeValue = myLibrary[i].toggleRead()});
             card.appendChild(button);
+            // To create div to place change status btn by the read status
+            let status = document.createElement("div");
+            status.classList.add("together");
+            status.appendChild(bookRead);
+            status.appendChild(button);
+            card.append(status);
             // Delete Book button
             let delButton = document.createElement("button");
-            let delText = document.createTextNode("Delete Book from Library")
+            delButton.classList.add("sec-btn");
+            let delText = document.createTextNode("Delete Book")
             delButton.appendChild(delText);
             delButton.addEventListener("click", function() {
                 myLibrary.splice(i, 1)
                 document.getElementById(i).remove();
+                console.table(myLibrary)
                 });
             card.appendChild(delButton);
 
